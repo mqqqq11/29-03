fdb3689966374c9eda83edd5a708e24de2e2d46b
Azamat <azamustapakylov66@gmail.com>
2023-08-13 22:25:31 +0600

    first commit

--- DZ3_Azamat_29-03/app.js
+++ DZ3_Azamat_29-03/app.js
@@ -1,22 +1,25 @@
-var newArray = [123, 234, 256, 345, 456, 567, 543, 67, 55, 23, 28, 50]
-for (var i = 0; i < newArray.length; i++) {
-    var str = String(newArray[i])
+//1
+var array = [123, 234, 256, 345, 456, 567, 543, 67, 55, 23, 28, 50]
+var newArray = []
+for (var i = 0; i < array.length; i++) {
+    var str = String(array[i])
     var backNum = Number(str[0])
     if (backNum === 2 || backNum === 5) {
-        console.log(newArray[i]);
+        newArray.push (array[i])
     }
 }
+console.log(newArray);
 
+
+//2
 var useFunc = function (type) {
     return typeof type
-    console.log(useFunc(false));
-    console.log(useFunc('HI'));
-    console.log(useFunc(88));
 }
+console.log(useFunc(false));
+console.log(useFunc('HI'));
+console.log(useFunc(88));
 
-
-
-
+//3
 var tags = ['li', 'div', 'li', 'p', 'h1', 'p', 'h1', 'div', 'div', 'li', 'h1',  'p', 'h1', 'ol', 'br']
 var tagSafe = {}
 for (var tag of tags) {
